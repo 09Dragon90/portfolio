7e2427dae4bd68a36d2e095695b68e22d536c228
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-03-30 15:16:55 +0200

    Dectivated router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [{ path: '', component: LandingPageComponent }];
+export const routes: Routes = [];
