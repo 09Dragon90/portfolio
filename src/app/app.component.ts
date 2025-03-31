110f7b97c1229ecbecdf522cae8579e87123f72b
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-03-31 13:38:42 +0200

    Save and load language at LocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, TranslateModule],
+  imports: [CommonModule, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
 
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('de');
-  }
+  constructor() {}
 }
--- src/app/landing-page/section-above-to-fold/header/header.component.ts
+++ src/app/landing-page/section-above-to-fold/header/header.component.ts
@@ -11,10 +11,30 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent {
   language: 'de' | 'en' = 'de';
-  constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService) {
+    this.loadLanguage();
+  }
 
   changeLanguage() {
     this.language = this.language == 'de' ? 'en' : 'de';
     this.translate.use(this.language);
+    this.saveLanguage();
+  }
+
+  saveLanguage() {
+    localStorage.setItem('language', this.language);
+  }
+
+  loadLanguage() {
+    let language = localStorage.getItem('language');
+    if (language == null) {
+      this.translate.setDefaultLang('de');
+      this.saveLanguage();
+    } else if (language == 'de' || language == 'en') {
+      this.translate.setDefaultLang(language);
+      this.language = language;
+    } else {
+      console.error('Wrong language');
+    }
   }
 }
