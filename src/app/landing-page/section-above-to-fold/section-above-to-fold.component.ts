a6763ea37d36af5d6995131c164969bd4cfa4d14
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-03-31 21:16:05 +0200

    above to fold finished

--- src/app/landing-page/section-above-to-fold/section-above-to-fold.component.ts
+++ src/app/landing-page/section-above-to-fold/section-above-to-fold.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-section-above-to-fold',
   standalone: true,
-  imports: [HeaderComponent, TranslateModule],
+  imports: [HeaderComponent, TranslateModule, CommonModule],
   templateUrl: './section-above-to-fold.component.html',
   styleUrl: './section-above-to-fold.component.scss',
 })
-export class SectionAboveToFoldComponent {}
+export class SectionAboveToFoldComponent {
+  datas = [
+    'Avalibale for remote work',
+    'Frontend Developer',
+    'Base in Munich',
+    'Open to Work',
+  ];
+}
