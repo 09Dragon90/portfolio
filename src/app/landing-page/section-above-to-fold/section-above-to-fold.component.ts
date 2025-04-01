076c137fa9122ea544e1b6035e5719296d09b28d
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-04-01 09:14:14 +0200

    Data Marquee added to i18n

--- src/app/landing-page/section-above-to-fold/section-above-to-fold.component.ts
+++ src/app/landing-page/section-above-to-fold/section-above-to-fold.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,4 +17,21 @@ export class SectionAboveToFoldComponent {
     'Base in Munich',
     'Open to Work',
   ];
+
+  messages: Record<string, string> = {};
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.loadMarquee(localStorage.getItem('language') || 'de');
+    this.translate.onLangChange.subscribe((event) => {
+      this.loadMarquee(event.lang);
+    });
+  }
+
+  loadMarquee(lang: string) {
+    this.translate.getTranslation(lang).subscribe((data) => {
+      this.messages = data['Marquee'] || {};
+    });
+  }
 }
