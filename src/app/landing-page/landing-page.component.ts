2c3f217bc614ba0157790d8889aa30c9a9b03f5f
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-04-01 13:30:41 +0200

    article about me added

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SectionAboveToFoldComponent } from './section-above-to-fold/section-above-to-fold.component';
+import { SectionAboutMeComponent } from './section-about-me/section-about-me.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [SectionAboveToFoldComponent],
+  imports: [SectionAboveToFoldComponent, SectionAboutMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
---
+++ src/app/landing-page/section-about-me/section-about-me.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-about-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-about-me.component.html',
+  styleUrl: './section-about-me.component.scss'
+})
+export class SectionAboutMeComponent {
+
+}
