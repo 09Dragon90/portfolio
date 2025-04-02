90d50bb83305af226a6b7bf3d7f382ac8295dfe7
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-04-02 15:34:17 +0200

    Skills changed and added translation

--- src/app/landing-page/section-skill-set/section-skill-set.component.ts
+++ src/app/landing-page/section-skill-set/section-skill-set.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PopupComponent } from './popup/popup.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-section-skill-set',
   standalone: true,
-  imports: [CommonModule],
+  imports: [PopupComponent, TranslateModule],
   templateUrl: './section-skill-set.component.html',
   styleUrl: './section-skill-set.component.scss',
 })
