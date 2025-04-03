4dea8b6661929e170b4486e38269f2818c627402
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-04-03 13:57:11 +0200

    Section projects changed

--- src/app/landing-page/section-projects/section-projects.component.ts
+++ src/app/landing-page/section-projects/section-projects.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+interface Project {
+  name: string;
+  technologies: string[];
+  images: string;
+  description: string;
+  github: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-section-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './section-projects.component.html',
-  styleUrl: './section-projects.component.scss'
+  styleUrl: './section-projects.component.scss',
 })
 export class SectionProjectsComponent {
+  @Input() index: number = 0;
+  projects: Project[] = [];
 
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.loadMarquee(localStorage.getItem('language') || 'de');
+    this.translate.onLangChange.subscribe((event) => {
+      this.loadMarquee(event.lang);
+    });
+  }
+
+  loadMarquee(lang: string) {
+    this.translate.getTranslation(lang).subscribe((data) => {
+      this.projects = [];
+      this.projects = data['Projects']['dataProjects'] || {};
+      console.log(this.projects[0].name);
+    });
+  }
 }
