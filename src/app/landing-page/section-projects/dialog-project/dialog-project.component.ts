ecff4200a83d2bf4ab5359ee701863c881cd655f
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-04-07 14:24:43 +0200

    dialog finished

--- src/app/landing-page/section-projects/dialog-project/dialog-project.component.ts
+++ src/app/landing-page/section-projects/dialog-project/dialog-project.component.ts
@@ -1,21 +1,27 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Project } from '../../../../assets/interfaces/project.interface';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dialog-project',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './dialog-project.component.html',
   styleUrl: './dialog-project.component.scss',
 })
 export class DialogProjectComponent {
   project: Project;
+  indexProject: number;
+  indexTitle: string = '';
 
   constructor(
+    private dialogRef: MatDialogRef<DialogProjectComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { projects: []; index: number }
   ) {
-    this.project = this.getProject(data.index);
+    this.indexProject = data.index;
+    this.project = this.getProject(this.indexProject);
+    this.getTitle(this.indexProject);
   }
 
   getProject(index: number) {
@@ -24,6 +30,17 @@ export class DialogProjectComponent {
 
   getTitle(index: number) {
     let number = index + 1;
-    return number.toString().padStart(2, '0');
+    this.indexTitle = number.toString().padStart(2, '0');
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  nextProject() {
+    let length = this.data.projects.length;
+    this.indexProject = (this.indexProject + 1) % length;
+    this.getTitle(this.indexProject);
+    this.project = this.getProject(this.indexProject);
   }
 }
