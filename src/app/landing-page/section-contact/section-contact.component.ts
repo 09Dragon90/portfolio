a470dea0e93cb9484a76c9579853092861079279
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-04-08 13:08:26 +0200

    Style contact form

--- src/app/landing-page/section-contact/section-contact.component.ts
+++ src/app/landing-page/section-contact/section-contact.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-section-contact',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormsModule],
   templateUrl: './section-contact.component.html',
   styleUrl: './section-contact.component.scss',
 })
-export class SectionContactComponent {}
+export class SectionContactComponent {
+  openPrivacyPolicy(e: any) {
+    e.preventDefault();
+  }
+  onSubmit() {}
+}
