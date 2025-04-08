f162e2d36a40f96fc7599a677fca8d43ac404ea3
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-04-08 21:07:35 +0200

    footer styled

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,6 +4,7 @@ import { SectionAboutMeComponent } from './section-about-me/section-about-me.com
 import { SectionSkillSetComponent } from './section-skill-set/section-skill-set.component';
 import { SectionProjectsComponent } from './section-projects/section-projects.component';
 import { SectionContactComponent } from './section-contact/section-contact.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -14,6 +15,7 @@ import { SectionContactComponent } from './section-contact/section-contact.compo
     SectionSkillSetComponent,
     SectionProjectsComponent,
     SectionContactComponent,
+    FooterComponent,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
