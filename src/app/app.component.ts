e8dde13c6737061464b622f5d4a9778342e6b0e5
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-04-08 21:47:40 +0200

    Router activated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Portfolio';
 
   constructor() {}
 }
