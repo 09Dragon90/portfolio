5c79d3ab8d306844d1d704a5b817e528d88e9128
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-04-22 12:08:47 +0200

    Translation Service change now HTML Attribute Lang

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
@@ -8,13 +9,17 @@ export class TranslationService {
   defaultLanguage: 'de' | 'en' = 'en';
   language: 'de' | 'en' = this.defaultLanguage;
 
-  constructor(private translate: TranslateService) {
+  constructor(
+    private translate: TranslateService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
     this.loadLanguage();
   }
 
   changeLanguage() {
     this.language = this.language == 'de' ? 'en' : 'de';
     this.translate.use(this.language);
+    this.setLangHTML(this.language);
     this.saveLanguage();
   }
 
@@ -30,12 +35,18 @@ export class TranslationService {
     let language = localStorage.getItem('language');
     if (language == null) {
       this.translate.setDefaultLang(this.defaultLanguage);
+      this.setLangHTML(this.defaultLanguage);
       this.saveLanguage();
     } else if (language == 'de' || language == 'en') {
       this.translate.setDefaultLang(language);
+      this.setLangHTML(language);
       this.language = language;
     } else {
       console.error('Wrong language');
     }
+  }
+
+  setLangHTML(lang: string): void {
+    this.document.documentElement.lang = lang;
   }
 }
