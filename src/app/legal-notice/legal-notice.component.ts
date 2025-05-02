2d749a53323c54d6bbca301dd50acaf8ab465489
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-05-02 16:48:18 +0200

    Header added

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,18 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { HeaderMobileComponent } from '../shared/header-mobile/header-mobile.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, HeaderComponent, HeaderMobileComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
 export class LegalNoticeComponent {
   translation = inject(TranslationService);
   currentLanguage: string;
-  constructor() {
+
+  constructor(private translate: TranslateService) {
     this.currentLanguage = this.translation.getLanguage();
+  }
+
+  ngOnInit() {
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang;
+    });
+  }
+
+  windowWidth: number = window.innerWidth;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.windowWidth = (event.target as Window).innerWidth;
+  }
+
+  isSmallScreen(): boolean {
+    return this.windowWidth > 780;
   }
 }
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,18 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { HeaderMobileComponent } from '../shared/header-mobile/header-mobile.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, HeaderComponent, HeaderMobileComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {
   translation = inject(TranslationService);
   currentLanguage: string;
-  constructor() {
+
+  constructor(private translate: TranslateService) {
     this.currentLanguage = this.translation.getLanguage();
+  }
+
+  ngOnInit() {
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang;
+    });
+  }
+
+  windowWidth: number = window.innerWidth;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.windowWidth = (event.target as Window).innerWidth;
+  }
+
+  isSmallScreen(): boolean {
+    return this.windowWidth > 780;
   }
 }
