48e7f7a9edfbaa9e08910bc48f5d5534e330fde0
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-05-03 19:39:10 +0200

    added animations to about me section

--- src/app/landing-page/section-about-me/section-about-me.component.ts
+++ src/app/landing-page/section-about-me/section-about-me.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { AnimationService } from '../../services/animation.service';
 
 @Component({
   selector: 'app-section-about-me',
@@ -8,4 +9,37 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './section-about-me.component.html',
   styleUrl: './section-about-me.component.scss',
 })
-export class SectionAboutMeComponent {}
+export class SectionAboutMeComponent {
+  sectionY = 0;
+  viewportHeight: Number;
+  constructor(private animations: AnimationService) {
+    this.viewportHeight = window.innerHeight;
+  }
+
+  // ngAfterViewInit() {
+  //   this.sectionY = this.animations.getYComponent('section-about-me');
+  // }
+
+  // ngOnInit() {
+  //   window.addEventListener('scroll', this.onScroll, true);
+  // }
+
+  // onScroll = () => {
+  //   const yOffset = window.scrollY;
+  //   console.log('Componente Y-Wert:', this.sectionY);
+  //   console.log('Aktueller Y-Wert:', yOffset);
+  //   console.log('Aktueller Y-Wert:', yOffset);
+  // };
+
+  // ngOnDestroy() {
+  //   window.removeEventListener('scroll', this.onScroll, true);
+  // }
+
+  @ViewChild('article') observeArticle!: ElementRef;
+  @ViewChild('imgProfile') observeImgProfile!: ElementRef;
+
+  ngAfterViewInit() {
+    this.animations.observeElement(this.observeArticle, 'animation-b-t');
+    this.animations.observeElement(this.observeImgProfile, 'animation-b-t');
+  }
+}
