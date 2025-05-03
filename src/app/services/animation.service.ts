8237d1c8ba1a38f39c6314a21eea4cee458d7ffe
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-05-03 19:38:51 +0200

    added service for trigger animations

---
+++ src/app/services/animation.service.ts
@@ -0,0 +1,32 @@
+import { ElementRef, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimationService {
+  constructor() {}
+
+  observeElement(element: ElementRef, animation: string) {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const el = element.nativeElement;
+          const isBelowViewport = entry.boundingClientRect.top > window.scrollY;
+
+          if (entry.isIntersecting) {
+            el.classList.add('start-' + animation);
+            el.classList.remove('reverse-' + animation);
+          } else if (isBelowViewport) {
+            el.classList.remove('start-' + animation);
+            el.classList.add('reverse-' + animation);
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    observer.observe(element.nativeElement);
+  }
+}
